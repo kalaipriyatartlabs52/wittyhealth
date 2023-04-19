5b8205eaad5d0a64d36f9a188ef8cc501b232cc7
nithyatartlabs <nithya.chandrasekaran@tartlabs.com>
2023-04-19 16:34:25 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+
+<title>The title of your website</title>
+<link rel="stylesheet" type="text/css" href="/witty/assets/css/style.css"></link>
+<script src="/witty/assets/js/main.js" type="text/javascript"></script>
+</head>
+
+<body>
+<?php include_once('header-part.php'); ?>
+<?php include_once('bottom-part.php'); ?>
+</body>
+
+</html>
