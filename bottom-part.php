2b30df5ffcc0a4a027066a04f6edb82335be8a6e
nithyatartlabs <nithya.chandrasekaran@tartlabs.com>
2023-04-20 18:38:35 +0530

    footer learning more founder section completed with responsive news section in progress

--- bottom-part.php
+++ bottom-part.php
@@ -1,1 +1,138 @@
-<p>Footer part</p>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+</head>
+<body>
+
+<!-- News section -->
+<section class="bg-grey pb-5">
+    <div class="container position-relative">
+        <div class="px-5 news-top">
+            <h2 class="pb-4">Witty Health in The News</h2>
+            <p>We believe that inclusivity and collaboration is essential in delivering both a<br> meaningful product to the user while working to drive down costs in <br>healthcare. Here are a few of the partnerships we've secured along the way.</p>
+        </div>
+        <div class="square-img1">
+        </div>
+        <div class="row">
+            <div class="col-4 news-card d-flex justify-content-center  align-items-center">
+                <img src="./assets/images/bloomberg-img.png" alt="bloomberg-img" class="object-fit-cover" width="292px" height="185px">
+            </div>
+            <div class="col-4 news-card d-flex justify-content-center  align-items-center">
+                <img src="./assets/images/yahoo-img.png" alt="yahoo-img" class="object-fit-cover" width="292px" height="185px">
+            </div>
+            <div class="col-4 news-card d-flex justify-content-center  align-items-center">
+             <img src="./assets/images/marketwatch-img.png" alt="marketwatch-img" class="object-fit-cover" width="292px" height="185px">
+            </div>
+
+        </div>
+    <div>
+
+
+</section>
+
+<!-- Founder section -->
+<section class="container mt-5 border-left">
+    <div class="d-flex flex-row gap-3 flex-wrap justify-content-lg-start justify-content-center">
+        <div class="">
+            <img src="./assets/images/ramsesha-img.png" alt="ramsesha-img" class="object-fit-cover" width="258px" height="265px">
+            <h6 class="px-3 pt-3 founder-name">Ram Sesha<br>Cofounder & CEO</h6>
+        </div>
+        <div class="">
+            <img src="./assets/images/michaelseidan-img.png" alt="michaelseidan-img" class="object-fit-cover" width="258px" height="265px">
+            <h6 class="px-3 pt-3 founder-name">Michael Seiden<br> Strategic advisor, Former<br> President of US Oncology</h2>
+        </div>
+        <div class="">
+            <img src="./assets/images/girl-img.png" alt="girl-img" class="object-fit-cover" width="258px" height="265px">
+            <h6 class="px-3 pt-3 founder-name">Name Name<br> Cofounder & COO</h6>
+        </div>  
+        <div class="">
+            <img src="./assets/images/rachelspencer-img.png" alt="rachelspencer-img" class="object-fit-cover" width="258px" height="265px"> 
+            <h6 class="px-3 pt-3 founder-name">Rachel Spencer<br>Director of nutrition</h6>
+        </div>    
+        <div class="">
+                <img src="./assets/images/keithmoore-img.png" alt="keithmoore-img" class="object-fit-cover" width="258px" height="265px"> 
+                <h6 class="px-3 pt-3 founder-name">Keith Moore <br>VP of product</h6>
+        </div>  
+    </div>
+
+</section>
+
+
+
+<!-- Contact us -->
+<section class="contact-top container px-2">
+    <div class="row align-items-center gx-lg-5 gy-5">
+        <div class="col-lg-9 col-12">
+            <div class="contact-bg">
+                <h3 class="contact-heading">Interested in learning more?</h3>
+                <p class="pt-5 contact-description">Contact us: info@wittyhealth.com</p>
+            </div>
+        </div>
+        <div class="col-lg-3 col-12">
+            <p class="text-uppercase fw-medium started-heading bg-square position-relative z-1 ml-6">Let’s Get Started</p>
+        <div>
+    </div>
+</section>
+
+
+
+<!-- Footer Section -->
+<section class=" px-2">
+    <div class="container">
+        <div class="d-flex flex-row flex-wrap justify-content-between footer-sec">
+            <div class="p-2">
+                <div class="pb-5 ">
+                    <img src="./assets/images/witty-logo.png" alt="witty-logo" class="object-fit-cover" width="125px" height="50px">
+                </div>
+                <div class="">
+                    <img src="./assets/images/facebook-logo.svg" alt="facebook-logo" class="object-fit-cover pr-4" width="19px" height="19px">
+                    <img src="./assets/images/twitter-logo.svg" alt="twitter-logo" class="object-fit-cover" width="19px" height="19px">
+                    <img src="./assets/images/linkedin-logo.svg" alt="linkedin-logo" class="object-fit-cover" width="19px" height="19px">
+                </div>
+            </div>
+            <div class="p-2">
+                <p>5850 San Felipe, Suite 500<br>Houston, TX 77057<br>
+                    office (713) 470-2580<br>fax 713-496-9264</p>
+            </div>
+            <div class="p-2">
+                <ul class="menu-list">
+                    <li class="our-team">Our Team</li>
+                    <li>About Us</li>
+                    <li>Our Team</li>
+                    <li>Products</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+            <div class="p-2">
+                <ul class="menu-list">
+                    <li>Facebook</li>
+                    <li>Twitter</li>
+                    <li>Linkedin</li>
+                <ul>
+            </div>
+            <div class="p-2">
+                <p>© Copyright 2023.<br>
+                 All Rights Reserved.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+</body>
--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 <title>The title of your website</title>
 <link rel="stylesheet" type="text/css" href="/witty/assets/css/style.css"></link>
 <script src="/witty/assets/js/main.js" type="text/javascript"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 
 <body>
